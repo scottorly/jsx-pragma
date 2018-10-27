5921e8682c9acba2c98b8ee65df3a8c84e467d6c
Scott Orlyck <=>
2018-10-27 13:34:06 -0700

    JSX -> DOM

---
+++ index.js
@@ -0,0 +1,75 @@
+export const f = ({ children }) => {
+    try {
+        const fragment = document.createDocumentFragment()
+        children.forEach(child => {
+            if (typeof child === 'string') {
+                fragment.appendChild(document.createTextNode(child))
+            } else if (child instanceof HTMLElement === true) {
+                fragment.appendChild(child)
+            } else if (child instanceof Array === true) {
+                child.forEach(item => fragment.appendChild(item))
+            }
+        })
+        return fragment
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const h = (elementName, attributes, ...args) => {
+    try {
+
+        let children = args.length ? [].concat(...args) : []
+
+        if (attributes === null || attributes === undefined) {
+            attributes = {}
+        }
+
+        if (typeof elementName === 'function') {
+            return elementName({
+                attributes,
+                children
+            })
+        }
+
+        const element = document.createElement(elementName)
+
+        if (attributes == null && children == undefined) {
+            return element
+        }
+
+        children.forEach(child => {
+            if (typeof child === 'string') {
+                element.appendChild(document.createTextNode(child))
+            } else if (child instanceof HTMLElement == true) {
+                element.appendChild(child)
+            } else if (child instanceof Array === true) {
+                child.forEach(item => element.appendChild(item))
+            }
+        })
+
+        Object.entries(attributes).forEach(([key, value]) => {
+            if (key !== 'className' && key !== 'eventListener' && key !== 'eventListeners') {
+                element.setAttribute(key, value)
+            }
+        })
+
+        const { className, eventListener, eventListeners } = attributes
+
+        if (className != undefined) {
+            className.split(" ").forEach(c => {
+                element.classList.add(c)
+            })
+        }
+        if(eventListener != undefined) {
+            element.addEventListener(...eventListener)
+        }
+        if (eventListeners != undefined) {
+            eventListeners.forEach(listener => element.addEventListener(...listener))
+        }
+
+        return element
+    } catch (error) {
+        console.log(error)
+    }
+}
