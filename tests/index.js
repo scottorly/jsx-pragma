3f80b16c97e9a88a81fab23f45fa55bfa1b9863c
Scott Orlyck <scott.orlyck+orly@gmail.com>
2019-11-25 08:28:24 -0800

    update tests

--- tests/index.js
+++ tests/index.js
@@ -7,24 +7,37 @@ jsdom()
 tape('jsx-pragma', test => {
     var div = pragma.h(
         'div',
-        {className: 'css', id: 'an-id'},
+        { className: 'css', id: 'an-id' },
         pragma.h('div'),
         [
             'a',
             'list',
             'of strings',
-            pragma.h('div', null, ['and other things'])
+            pragma.h('div', null, ['and other elements'])
         ],
         pragma.h('span')
-        )
+    )
     test.ok(div instanceof HTMLDivElement)
     test.ok(div.classList.contains('css'))
     test.equal(div.id, 'an-id')
+    test.equal([...div.children].length, 3)
 
-    var svg = pragma.h('svg', { xmlns: 'http://www.w3.org/2000/svg' })
-    test.equal(svg.namespaceURI, 'http://www.w3.org/2000/svg')
+    test.equal(
+        pragma.h('div', null, 'a string').textContent,
+        'a string'
+    )
 
-    var fragment = pragma.f({children: [div, svg]})
+    test.equal(
+        pragma.h('div', null, ['a string', 'another one']).textContent,
+        ['a string', 'another one'].join('')
+    )
+
+    test.equal(
+        pragma.h('svg', { xmlns: 'http://www.w3.org/2000/svg' }).namespaceURI,
+        'http://www.w3.org/2000/svg'
+    )
+
+    var fragment = pragma.f({ children: [div, pragma.h('div')] })
     test.ok(fragment instanceof DocumentFragment)
 
     test.end()
